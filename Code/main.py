a25d3f4a6be49a7b1c32b3f606b624f7951d2166
LynhMin <namv2704@gmail.com>
2024-09-23 10:08:09 +0700

    Initial commit

---
+++ Code/main.py
@@ -0,0 +1,29 @@
+import pygame, sys
+from settings import *
+from level import Level
+
+#import pygame, sys, everything from settings
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('Sprout Land demo')
+        self.clock = pygame.time.Clock()
+        self.level = Level()
+
+    def run(self):
+        while(True):
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            #dt: DeltaTime
+            dt = self.clock.tick() / 1000
+            self.level.run(dt)
+            pygame.display.update()
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ Code/settings.py
@@ -0,0 +1,6 @@
+from pygame.math import Vector2
+
+#screen
+SCREEN_WIDTH = 1080
+SCREEN_HEIGHT = 720
+TILE_SIZE = 64
