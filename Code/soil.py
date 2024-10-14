7bd59262a2194acca20a8eee8ea07a61ed55034e
ogthmi <ongminh.cbg@gmail.com>
2024-10-14 21:15:26 +0700

    Change soil tiles

--- Code/soil.py
+++ Code/soil.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
 from pytmx.util_pygame import load_pygame
+from support import *
 
 class SoilLayer:
     def __init__(self, all_sprites):
@@ -10,6 +11,8 @@ class SoilLayer:
         self.soil_sprites = pygame.sprite.Group()
 
         #graphic
+        self.soil_surfs = import_folder_dict("../graphics/soil/")
+        # print(self.soil_surfaces)
         self.soil_surf = pygame.image.load("../graphics/soil/o.png")
         self.create_soil_grid()
         self.creat_hit_rects()
@@ -57,9 +60,71 @@ class SoilLayer:
         for index_row, row in enumerate(self.grid):
             for index_col, cell in enumerate(row):
                 if 'X' in cell:
+
+
+                    # 4 ô đất trên dưới -trái phải hiền với ô hiện tại
+                    top = 'X' in self.grid[index_row - 1][index_col]
+                    bottom = 'X' in self.grid[index_row + 1][index_col]
+                    left = 'X' in row[index_col - 1]
+                    right = 'X' in row[index_col + 1]
+
+                    tile_type = 'o' # mặc định (ô đất có viền bo tròn 4 cạnh)
+
+                    # kiểm tra các ô trên dưới - trái phải đã tạo đất trồng chưa để chọn đúng hình ảnh ô đất
+                    # nếu toàn bộ trên duới trái phải là đất
+                    if all((top, right, left, bottom)):
+                        tile_type = 'x' #ô giữa trong bảng 3x3
+
+                    # nếu ô bên trái là đất (còn lại thì không)
+                    if left and not any((top, bottom, right)):
+                        tile_type = 'r' # ô ngoài cùng bên phải
+                    # nếu ô bên phải là đất (còn lại thì không)
+                    if right and not any((top, bottom, left)):
+                        tile_type = 'l' # ô ngoài cùng bên trái
+                    #nếu 2 ô trái phải là đất (trên dưới thì không)
+                    if left and right and not any((top, bottom)):
+                        tile_type = 'lr' # ô ở giữa 1 hàng
+
+                    # nếu ô bên trên là đất (còn lại thì không)
+                    if top and not any((bottom, left, right)):
+                        tile_type = 'b'  # ô dưới cùng
+                    # nếu ô bên dưới là đất (còn lại thì không)
+                    if bottom and not any((top, left, right)):
+                         tile_type = 't'  # ô trên cùng
+                    # nếu 2 ô trên dưới là đất (trái phải thì không)
+                    if top and bottom and not any((left, right)):
+                        tile_type = 'tb'  # ô ở giữa 1 cột
+
+                    # nếu 2 ô trái và dưới là đất (còn lại thì không)
+                    if left and bottom and not any((top, right)):
+                        tile_type = 'tr'  # ô trên cùng bên phải
+                    # nếu 2 ô trái và trên là đất (còn lại thì không)
+                    if left and top and not any((bottom, right)):
+                        tile_type = 'br'  # ô dưới cùng bên phải
+                    # nếu 2 ô phải và dưới là đất (còn lại thì không)
+                    if right and bottom and not any((top, left)):
+                        tile_type = 'tl'  # ô trên cùng bên trái
+                    # nếu 2 ô phải và trên là đất (còn lại thì không)
+                    if right and top and not any((bottom, left)):
+                         tile_type = 'bl'  # ô trên cùng bên trái
+
+                    # nếu các ô đát tạo thành hình chữ T (|-)
+                    if all ((top, bottom, right)) and not left:
+                        tile_type = 'tbr'
+                    # nếu các ô đát tạo thành hình chữ T (-|)
+                    if all((top, bottom, left)) and not right:
+                        tile_type = 'tbl'
+                    # nếu các ô đát tạo thành hình chữ T xuôi
+                    if all((left, right, bottom)) and not top:
+                         tile_type = 'lrt'
+                    # nếu các ô đát tạo thành hình chữ T ngược xuồng dưới
+                    if all((left, right, top)) and not bottom:
+                        tile_type = 'lrb'
+
+
                     SoilTile(
                         pos = (index_col * TILE_SIZE, index_row * TILE_SIZE),
-                        surf = self.soil_surf,
+                        surf = self.soil_surfs[tile_type],
                         groups = [self.all_sprites, self.soil_sprites]
                     )
 
